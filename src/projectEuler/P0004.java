c707d0742d466eda2244489fefb8880353f295e5
meta_system <meta_system@online.degit config --global user.email meta_system@online.degit config --global user.email meta_system@online.de>
2015-09-13 01:41:58 +0200

    Fixed P0004

--- src/projectEuler/P0004.java
+++ src/projectEuler/P0004.java
@@ -1,36 +1,41 @@
 package projectEuler;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class P0004 {
 
+	
+	
 	public static void main(String[] args) {
-
+		
+		Queue<Integer> myPalindromes = new LinkedList<Integer>();
+		
 		int result = 0;
-		boolean hasResult = false;
-
-		for (int a = 999; a > 100; a--){
+		
+		for (int a = 999; a > 99; a--){
 			for (int b = 999; b > a; b--){
 				result = a*b;
 				if (isPalindrome(result)){
-					hasResult = true;
-					break;
+					System.out.println(a + "*" + b + " = " +result);
+					
+					myPalindromes.add(result);
 				}
 
 			}
-			if (hasResult){
-				break;
-			}
+			
 		}
-
-		System.out.println("result: " + result);
+		result = Collections.max(myPalindromes);
+		System.out.println("result: " + result );
 
 	}
 
 	public static boolean isPalindrome(int number){
 		String numberS = String.valueOf(number);
-		System.out.println(numberS);
 		boolean result = true;
 		for (int i = 0; i < (numberS.length()/2); i++){
-			if (numberS.charAt(i) != numberS.charAt(numberS.length()-i)){
+			if (numberS.charAt(i) != numberS.charAt(numberS.length()-i-1)){
 				result = false;
 			}
 		}
