41499d753c26bdaa55cabfae7950d8c4a459ace4
meta_system <meta_system@online.degit config --global user.email meta_system@online.degit config --global user.email meta_system@online.de>
2015-09-13 22:58:31 +0200

    Added P0009(working) and P0010(working)

--- src/projectEuler/P0009.java
+++ src/projectEuler/P0009.java
@@ -1,10 +1,61 @@
 package projectEuler;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class P0009 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*
+		 *Setze  	a := m2 - n2
+		 * und	b := 2mn 	
+		 * Dann ist: 	c2 	= a2 + b2
+		 * 	= (m2 - n2)2 + (2mn)2
+		 * 	= m4 - 2m2n2 + n4 + 4m2n2
+		 * 	= m4 + 2m2n2 + n4
+		 * 	= (m2 + n2)2
+		 * und	c	= m2 + n2
+		 * 
+		 * Bei unserem Problem:
+		 * 
+		 * a + b + c = 1000
+		 * 
+		 * Setze für a und b ein
+		 * 
+		 * 2m2 + 2mn = 1000
+		 * 2m(m+n) = 1000
+		 * m+n = 500/m
+		 * n = 500/m - m
+		 * 
+		 * Also nun nur stur a und b ausprobieren!
+		 */
+		int number = 1000;
+		int myNumber = number/2;
+		int n = 0, b = 0, a = 0, result = 0, m;
+		Queue<Integer> factors = new LinkedList<Integer>();
+		
+		for(int i = 1; i <= 500; i++){
+			if (myNumber%i == 0){
+				factors.add(i);
+			}
+		}
+		
+		while (factors.peek()!=null){
+			m = factors.peek();
+			n = myNumber/m-m;
+			b = 2 * m * n;
+			a = (m*m)-(n*n);
+			if((0 < n)&&(0<b)&&(0<a)){
+				System.out.println(m);
+				result = a * b * (m*m+n*n);
+				System.out.println("result: " + result);
+			}
+				factors.poll();
+			
+		}
+		
+		
 	}
 
 }
---
+++ src/projectEuler/P0010.java
@@ -0,0 +1,73 @@
+package projectEuler;
+
+public class P0010 {
+
+	public static void main(String[] args) {
+		long prim = 3; 				//zu prüfende Zahl
+		long sum = 2;
+		int count = 1;
+		
+		long startTime = System.currentTimeMillis();
+		
+		while(prim < 2000000){
+			if (isPrime(prim)){
+				sum += prim;
+				count ++;
+			}
+			prim += 2;
+		}
+	
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		double time = (totalTime /1000.0);
+		System.out.println("time in secs " + time);
+		System.out.println("count " + count);
+		System.out.println("sum of primes below 200000\n" + sum);
+		
+		
+		
+		long[] pZahlen;
+		pZahlen = new long[2000000];
+		prim = 3; 				//zu prüfende Zahl
+		sum = 2;
+		pZahlen[0] = 2;
+		pZahlen[1] = 3;
+		
+		startTime = System.currentTimeMillis();
+		
+		for (int i = 1; i< pZahlen.length;){
+			if (isPrime(prim)){
+				pZahlen[i] = prim;
+				sum += prim;
+				i++;
+			}
+			prim += 2;
+		}
+		
+		endTime   = System.currentTimeMillis();
+		totalTime = endTime - startTime;
+		time = (totalTime /1000.0);
+		System.out.println("\ntime in secs " + time);
+		System.out.println("count " + 2000000);
+		System.out.println("sum of first 2000000 primes\n" + sum);
+
+	}
+	
+	public static boolean isPrime (long number){
+
+		boolean result = true;
+
+		for (long i = 2; i <= Math.sqrt(number); i+=2){
+			
+			if ((number % i)==0){
+				return false;
+			}
+			if(i ==2){
+				i -= 1;
+			}
+		}
+
+	//	System.out.println("prime2 " + number + " " + result);
+		return result;
+	}
+}
