a51d4624b8aeffe60d3c0882867bcfed2c0b62e5
meta_system <meta_system@online.degit config --global user.email meta_system@online.degit config --global user.email meta_system@online.de>
2015-09-13 17:39:54 +0200

    Streamlined P0007 and added it to ToC

--- src/projectEuler/P0007.java
+++ src/projectEuler/P0007.java
@@ -5,13 +5,12 @@ public class P0007 {
 	public static void main(String[] args) {
 		long[] pZahlen;
 		pZahlen = new long[10001];
-		int temp = 1;
-		long prim = 3; //zu prüfende Zahl
+		long prim = 3; 				//zu prüfende Zahl
+		long result = 0; 			// solution
+
 		pZahlen[0] = 2;
 		pZahlen[1] = 3;
-		temp = 1;
-		long result = 0;
-
+		
 		for (int i = 1; i< pZahlen.length;){
 			if (isPrime(prim)){
 				pZahlen[i] = prim;
