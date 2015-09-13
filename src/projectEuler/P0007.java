163827efdd0ddf675b3381197d4ccf2e391b52f0
meta_system <meta_system@online.degit config --global user.email meta_system@online.degit config --global user.email meta_system@online.de>
2015-09-13 17:37:08 +0200

    Fixed P0007

--- src/projectEuler/P0007.java
+++ src/projectEuler/P0007.java
@@ -4,12 +4,13 @@ public class P0007 {
 
 	public static void main(String[] args) {
 		long[] pZahlen;
-		pZahlen = new long[10002];
+		pZahlen = new long[10001];
 		int temp = 1;
 		long prim = 3; //zu prüfende Zahl
 		pZahlen[0] = 2;
 		pZahlen[1] = 3;
 		temp = 1;
+		long result = 0;
 
 		for (int i = 1; i< pZahlen.length;){
 			if (isPrime(prim)){
@@ -20,9 +21,8 @@ public class P0007 {
 			prim += 2;
 		}
 	
-		System.out.println(pZahlen[10000]);
-		System.out.println(pZahlen[1]);
-		System.out.println(pZahlen[2]);
+		result = pZahlen[10000];
+		System.out.println("result: " + result );
 
 	}
 	
@@ -30,7 +30,7 @@ public class P0007 {
 
 		boolean result = true;
 
-		for (long i = 2; i < Math.sqrt(number); i++){
+		for (long i = 2; i <= Math.sqrt(number); i++){
 			if ((number % i)==0){
 				return false;
 			}
