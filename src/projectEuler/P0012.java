2ad8fb45220153cee9fb18827d5d03b5c34e2583
meta_system <meta_system@online.degit config --global user.email meta_system@online.degit config --global user.email meta_system@online.de>
2015-09-17 19:40:45 +0200

    Added P0012(working)

---
+++ src/projectEuler/P0012.java
@@ -0,0 +1,51 @@
+package projectEuler;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class P0012 {
+
+	public static void main(String[] args) {
+
+		int divCount = 0;
+		int highCount = 0;
+		int goalCount= 500;
+		long result = 1;
+		Queue<Long> divisors = new LinkedList<Long>();
+		
+		int i = 1;
+		
+		
+		while (true){
+			divCount = 0;
+			i++;
+			result += i;
+
+			for (long j = 1; j <= result; j++){
+				if (result % j == 0){
+					divCount++;
+					divisors.add(j);
+				}
+				
+			}
+			if (divCount > highCount){
+				highCount = divCount;
+			}
+			if(i%100 == 0){
+				System.out.println("done " + i +" " + highCount + " "  );
+			}
+			if(divCount >= goalCount){
+				break;
+			} else {
+				divisors.clear();
+			}
+		}
+
+		System.out.println(result);
+		while(divisors.peek() != null){
+			System.out.println(divisors.poll());
+		}
+
+	}
+}
