8ead93776af8b4e2441b615c22159ab36db68997
meta_system <meta_system@online.degit config --global user.email meta_system@online.degit config --global user.email meta_system@online.de>
2015-09-17 23:23:13 +0200

    Finished P0014(working)

--- src/projectEuler/P0014.java
+++ src/projectEuler/P0014.java
@@ -3,7 +3,33 @@ package projectEuler;
 public class P0014 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		long start = 0;
+		long cur = 0;
+		long result = 0;
+		long bigStart = 0;
+		long bigResult = 0;
+
+		while (start < 1000000){
+			cur = ++start;
+			result = 1;
+			while(cur > 1){
+				if (cur%2 ==0){
+					cur /= 2;
+				} else {
+					cur = cur * 3 + 1;
+				}
+				result++;
+			}
+			result ++;
+			if(result > bigResult){
+				bigResult = result;
+				bigStart = start;
+			}
+
+		}
+		System.out.println("length of chain: " + bigResult + "\ncaused by starting number: " + bigStart);
 
 	}
 
